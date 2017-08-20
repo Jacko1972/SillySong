20d5d3952b9bb66a83793241aeda0466d6b30b0c
Andrew Jackson <jacko_clan@hotmail.com>
2017-08-20 20:41:51 +0100

    Code changes to meet rubric

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -21,23 +21,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var lyricsView: UITextView!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         nameField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func reset(_ sender: Any) {
         nameField.text = "";
         lyricsView.text = "";
     }
-
+    
     @IBAction func displayLyrics(_ sender: Any) {
         lyricsView.text = lyricsByName(template: bananaFanaTemplate, fullName: nameField.text!)
     }
@@ -55,11 +55,13 @@ func shortNameFromName(name: String) -> String {
     
     let range = tempName.startIndex..<tempName.index(after: tempName.startIndex)
     var removeChars: Bool = true
-    while removeChars {
-        if tempName.rangeOfCharacter(from: chars, options: String.CompareOptions.literal, range: range) != nil {
-            removeChars = false
-        } else {
-            tempName.remove(at: tempName.startIndex)
+    if tempName.rangeOfCharacter(from: chars, options: String.CompareOptions.literal, range: tempName.startIndex..<tempName.endIndex) != nil {
+        while removeChars {
+            if tempName.rangeOfCharacter(from: chars, options: String.CompareOptions.literal, range: range) != nil {
+                removeChars = false
+            } else {
+                tempName.remove(at: tempName.startIndex)
+            }
         }
     }
     return tempName
